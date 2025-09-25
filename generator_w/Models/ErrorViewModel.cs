5a562ca8fd92b3cd03a404ce967bdc0a189f6a02
Çağrı Tükenmez <grfspear@gmail.com>
2025-09-25 17:00:03 +0300

    1
    1

--- generator_w/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace generator_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
