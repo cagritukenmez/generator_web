8f0d2cd620a5eb121a5c8f8d405b2b7187bbce2b
Deldeserteur <a.damienadjalte@gmail.com>
2025-09-29 17:07:51 +0300

    sdfghjkl

--- generator_web/obj/Debug/net8.0/generator_web.AssemblyInfo.cs
+++ generator_web/obj/Debug/net8.0/generator_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09e7d5aeea87dc15ad35ba0904c8d46738c2d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8ad9866495f274063bbb68c0440aae741b599f")]
 [assembly: System.Reflection.AssemblyProductAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("generator_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
